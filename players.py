851f6ffb7cde0f67befdd99d4f5c9f0c8afcc9eb
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-05-06 16:35:00 -0700

    Implement iterative_deepening_search function

--- players.py
+++ players.py
@@ -1,6 +1,7 @@
 '''
     Defines Player class, and subclasses Human and Minimax Player.
 '''
+import time
 
 class Player:
     def __init__(self, symbol):
@@ -37,17 +38,45 @@ class MinimaxPlayer(Player):
             self.oppSym = 'O'
         else:
             self.oppSym = 'X'
+        self.MAXTIME = 2 # seconds
         self.tot_time = 0
 
-    def utility():
+    # Ref: https://courses.cs.washington.edu/courses/cse573/04au/Project/mini1/RUSSIA/Final_Paper.pdf
+    def utility(self):
         # Determines "Goodness" of a terminal state
         return
     
-    def successor():
+    def successor(self):
         # Takes in a current state and generates all succcessor states within one move
         return
+    
+    def iterative_deepening_search(self):
+        # Ref: https://stacks.stanford.edu/file/druid:wk764yw7162/wk764yw7162.pdf
+        # Adapted from previous assignment
 
-    def minimax(int depth):
+        # Use time as depth according to the assignment's terms
+        init_time = time.time()
+
+        for depth in range(1, 100):
+            result = self.dls(depth)
+            run_time = time.time() - init_time
+            if init_time > self.MAXTIME:
+                self.tot_time += run_time
+                # If it goes over two seconds, call the heuristic function to pick an answer
+                result = self.heuristic()
+                return result
+            if result != "cutoff":
+                return result
+            
+    def dls(self, depth):
+        return
+
+    def heuristic(self):
+        # https://courses.cs.washington.edu/courses/cse573/04au/Project/mini1/RUSSIA/Final_Paper.pdf
+        # 100 * (Max Players coins - Min Players coins) / (Max Players coins + Min Players coins)
+        return
+    
+    def minimax(depth):
         # Have heuristic function to make decision if past depth limit (2 sec)
         # override player class get_move inorder to produce a move through minimax
         return
